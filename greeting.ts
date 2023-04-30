56329cc9ebcea49b582b5cd465ca2a56ab8f9d3d
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-04-30 18:52:36 -0500

    add hours minutes and seconds in the greeting

--- greeting.ts
+++ greeting.ts
@@ -1,6 +1,10 @@
 import { Handler, APIGatewayProxyEvent, Context } from 'aws-lambda';
 export const hello: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
+    const date: Date = new Date ();
+    const hours: number = date.getHours();
+    const minutes: number = date.getMinutes();
+    const seconds: number = date.getSeconds();
     return {
-        "statusCode": 200, "body": JSON.stringify({message: "Hello world xdxd"})
+        "statusCode": 200, "body": JSON.stringify({message: `Hora: ${hours}:${minutes}:${seconds}`})
     }
 }
