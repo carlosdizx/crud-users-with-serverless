240092f6f7466ff5370144cd44d2af38cb3c5186
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-01 12:29:31 -0500

    refactor function for get parameter by url and search in the table with this data

--- getUsers.ts
+++ getUsers.ts
@@ -1,16 +1,22 @@
 import { Handler, APIGatewayProxyEvent, Context } from 'aws-lambda';
 import AWS from "aws-sdk";
 
-const dynamodb: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
-    accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
-    secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
-    });
+const dynamoDBClientParams = process.env.IS_OFFLINE ?
+    {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+        accessKeyId: 'DEFAULT_ACCESS_KEY',
+        secretAccessKey: 'DEFAULT_SECRET'
+    } :
+    {};
+
+const dynamodb: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient(dynamoDBClientParams);
 export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
 
+    const {id: userId}: any = event.pathParameters;
+
     const params = {
-        ExpressionAttributeValues: {':pk':'1'},
+        ExpressionAttributeValues: {':pk': userId},
         KeyConditionExpression: 'pk = :pk',
         TableName: 'users'
     };
