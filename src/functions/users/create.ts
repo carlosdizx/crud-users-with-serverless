f221a87a8252caf9b9ec09e35a6164847563aa40
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-06 00:56:14 -0500

    implement new util

--- src/functions/users/create.ts
+++ src/functions/users/create.ts
@@ -1,19 +1,12 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import UserService from "../../services/user.service";
-
-const { randomUUID } = require("crypto");
+import responseObject from "../../utils/Response";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   if (typeof event.body === 'string') {
     const body: any = JSON.parse(event.body);
     const result = await UserService.create(body);
-    return {
-      statusCode: 201,
-      body: JSON.stringify(result),
-    };
+    return responseObject(201, result);
   }
-  return {
-    statusCode: 400,
-    body: JSON.stringify({ msg: "Body is required" }),
-  };
+  return responseObject(201, "Body is required");
 };
